294eb2f5cba3396a1823dee89d6aebea95f47725
Blake Stewart <kanssdev@gmail.com>
2024-09-22 20:02:37 +0000

    Requirements satisfied

--- setup.sh
+++ setup.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
+sudo apt update
+sudo apt install gpsd
+gpsd --version
+sudo apt install python3
+python3 --version
+sudo apt install python3-pip
+pip3 --version
+sudo apt install socat
 
-pip install --trusted-host files.pythonhosted.org --trusted-host pypi.org --trusted-host pypi.python.org dynaconf
+echo "apt-get requirements satisfied"
+
+pip install -r requirements.txt --trusted-host files.pythonhosted.org --trusted-host pypi.org --trusted-host pypi.python.org
+
+echo "pip requirements satisfied. You should be good to go!"
